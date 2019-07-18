0435cab3f8300ed491f74f987546449184286758
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-07-19 00:53:12 +0200

    Auth and login setup

---
+++ OnTheMap/Controller/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Ahmed Afifi on 7/18/19.
+//  Copyright © 2019 Ahmed Afifi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        emailTextField.borderStyle = .roundedRect
+        passwordTextField.borderStyle = .roundedRect
+        loginButton.layer.cornerRadius = 5
+        
+        subscribeToNotification(UIResponder.keyboardWillShowNotification, selector: #selector(keyboardWillShow))
+        subscribeToNotification(UIResponder.keyboardWillHideNotification, selector: #selector(keyboardWillHide))
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromAllNotifications()
+    }
+    
+    func subscribeToNotification(_ notification: NSNotification.Name, selector: Selector) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: nil)
+    }
+    
+    func unsubscribeFromAllNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: Show/Hide Keyboard
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if keyboardHeight(notification) > 400 {
+            view.frame.origin.y = -keyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func keyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = (notification as NSNotification).userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func resignIfFirstResponder(_ textField: UITextField) {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        }
+    }
+
+}
+
